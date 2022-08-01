ba983fa5fc391fb61cc6c2e0bb90dda26829419a
Nakatai-0322 <nakatai0322@gmail.com>
2022-08-01 17:00:11 +0900

    Update NotFound.vue

--- src/.vuepress/theme/layouts/NotFound.vue
+++ src/.vuepress/theme/layouts/NotFound.vue
@@ -1,12 +1,10 @@
 <template>
     <div class="main-div not-found">
         <h1>404 Not Found</h1>
-
         <p>
             {{ $themeConfig.lang.notFound }}
         </p>
-
-        <img src="http://http.cat/404" alt="404 Cat">
+        <img src="https://http.cat/404" alt="404 Cat">
     </div>
 </template>
 
@@ -16,7 +14,8 @@ export default {
 }
 </script>
 
-<style lang="stylus" scoped>
-.not-found
-  text-align center
+<style scoped>
+.not-found {
+    text-align: center
+}
 </style>
